3372c61b837fea9ca5955d73248a566d019f557c
Anna <annatran93@gmail.com>
2019-11-28 10:30:15 -0500

    noice change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>welcome to my app</h3>
         </a>
       </header>
     </div>
