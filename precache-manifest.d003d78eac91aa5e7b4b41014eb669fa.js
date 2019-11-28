cac6c1457a5e4f07d81ebf6d1f72d1670235a2d6
Anna <annatran93@gmail.com>
2019-11-28 10:54:10 -0500

    Updates

---
+++ precache-manifest.d003d78eac91aa5e7b4b41014eb669fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c747e59f1bf5f8f46f2c6403c2c63863",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "abf8cfdca453f82775b6",
+    "url": "/gh-pages-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/gh-pages-app/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "abf8cfdca453f82775b6",
+    "url": "/gh-pages-app/static/js/main.420aeadf.chunk.js"
+  },
+  {
+    "revision": "2cb596bcb00809b477e3",
+    "url": "/gh-pages-app/static/js/runtime-main.2ed0f26c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/gh-pages-app/static/media/logo.25bf045c.svg"
+  }
+]);
